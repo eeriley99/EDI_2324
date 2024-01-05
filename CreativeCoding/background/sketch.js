f576c8dfb3885285eb61fb4ce554c1752aa88856
Erin E. Riley <eeriley99@gmail.com>
2024-01-05 11:00:57 -0500

    background

---
+++ CreativeCoding/background/sketch.js
@@ -0,0 +1,8 @@
+function setup() {//runs one time
+createCanvas (windowWidth, windowHeight);//canvas sized to window
+background ('red');//red background
+}
+
+function draw() {// runs in a loop
+
+}
