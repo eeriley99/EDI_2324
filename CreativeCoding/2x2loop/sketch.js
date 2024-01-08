a54cde5c1c2bac08424ab1275ee0aeeaeac32594
Erin E. Riley <eeriley99@gmail.com>
2024-01-08 13:53:59 -0500

    2x2 loop

---
+++ CreativeCoding/2x2loop/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  //runs one time
+  createCanvas(720, 720);
+  background(255, 0, 0);
+  fill("yellow");
+  strokeWeight(8);
+}
+
+function draw() {
+  //runs in a loop
+
+  translate(60, 60); //resets origin point
+
+  for (var y = 0; y < 600; y = y + 300) {
+    //loop to create a grid in the y direction
+
+    for (var x = 0; x < 600; x = x + 300) {
+      //loop to create a row of squares in the x direction
+      quad(x, y, x + 300, y, x + 300, y + 300, x, y + 300);
+    }
+  }
+}
