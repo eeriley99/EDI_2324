3dc7e7235076c6216851537a1641677769e2af9a
Erin E. Riley <eeriley99@gmail.com>
2024-01-10 12:48:25 -0500

    gridGIF

---
+++ CreativeCoding/test/GIFgrid/sketch.js
@@ -0,0 +1,31 @@
+let gif;
+
+function preload (){
+gif = loadImage ('assets/RileyGIF.gif');
+}
+
+function setup() {
+  //runs one time
+  createCanvas(windowWidth, windowHeight);
+  fill('purple');
+  strokeWeight(1);
+}
+
+function draw() {
+  //runs in a loop
+  
+  var num = 50;
+  var sideLen = windowWidth/num;
+
+  for (var y = 0; y < windowHeight; y = y + sideLen) {
+    //loop to create a grid in the y direction
+
+    for (var x = 0; x < windowWidth; x = x + sideLen) {
+image (gif, x, y, windowWidth/num, windowWidth/num);
+    }
+  }
+}
+
+function windowResized(){
+  resizeCanvas (windowWidth, windowHeight);
+}
--- CreativeCoding/tuningWvariables/sketch.js
+++ CreativeCoding/tuningWvariables/sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   //runs one time
   createCanvas(windowWidth, windowHeight);
-  fill('purple');
+  fill('magenta');
   strokeWeight(1);
 }
 
