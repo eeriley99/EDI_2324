a823bb6cec5e78f54fd561a7ee7c0af97bad0d7b
Erin E. Riley <eeriley99@gmail.com>
2024-01-10 11:31:07 -0500

    update

--- CreativeCoding/variables_GIF/sketch.js
+++ CreativeCoding/variables_GIF/sketch.js
@@ -7,12 +7,14 @@ gif = loadImage ('assets/RileyGIF.gif');
 function setup() {
   //runs one time
   createCanvas(windowWidth, windowHeight);
-  fill('red');
+  fill("yellow");
   strokeWeight(1);
 }
 
 function draw() {
   //runs in a loop
+  
+  background ('red');
   
   var num = 50;
   var sideLen = windowWidth/num;
@@ -21,7 +23,10 @@ function draw() {
     //loop to create a grid in the y direction
 
     for (var x = 0; x < windowWidth; x = x + sideLen) {
-    image (gif, x, y, sideLen, sideLen);
+    
+    image (gif, x, y, windowWidth/num, windowWidth/num);
+    
+    }
   }
 }
 
